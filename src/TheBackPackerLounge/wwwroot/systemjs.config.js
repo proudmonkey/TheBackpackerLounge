f5117cbbef9793840c5462f35fae0c313213d413
Vincent Maverick S Durano <vmsdurano@live.com.ph>
2016-09-07 05:47:53 -0700

    updated systemjs

--- src/TheBackPackerLounge/wwwroot/systemjs.config.js
+++ src/TheBackPackerLounge/wwwroot/systemjs.config.js
@@ -2,7 +2,7 @@
     System.config({
         paths: {
             // paths serve as alias
-            'npm:': 'node_modules/'
+            'npm:': 'js/'
         },
         // map tells the System loader where to look for things
         map: {
